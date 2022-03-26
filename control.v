75725550b948729619b526445d61b33343587dc6
Utkarsh M <utkarshrm568@gmail.com>
2022-03-26 05:54:11 +0530

    adding addi instruction

--- control.v
+++ control.v
@@ -19,6 +19,7 @@ always @(opcode) begin
 		6'b101011:{RegDst, ALUSrc, MemtoReg, RegWrite, MemRead, MemWrite, Branch, AluOP, Ne}=10'bx1x001_0_00_x; //sw
 		6'b000100:{RegDst, ALUSrc, MemtoReg, RegWrite, MemRead, MemWrite, Branch, AluOP, Ne}=10'bx0x000_1_01_0; //beq
 		6'b000101:{RegDst, ALUSrc, MemtoReg, RegWrite, MemRead, MemWrite, Branch, AluOP, Ne}=10'bx0x000_1_01_1; //bne
+		6'b001000:{RegDst, ALUSrc, MemtoReg, RegWrite, MemRead, MemWrite, Branch, AluOP, Ne}=10'b010100_0_00_x; //addi
 		default:
 	{RegDst,ALUSrc,MemtoReg,RegWrite,MemRead,MemWrite,Branch,AluOP}=9'bxxx_xxx_x_xx;
 	endcase
